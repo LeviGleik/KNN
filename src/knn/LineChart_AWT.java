3fd411a548d3f69143578af3d400f060ed3f9b82
levig <levig@DESKTOP-L6J83CI>
2019-05-05 12:11:59 -0300

    Gráfico sem revisão

---
+++ src/knn/LineChart_AWT.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knn;
+
+import java.awt.BasicStroke;
+import java.awt.Color; 
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.ApplicationFrame;
+
+/**
+ *
+ * @author 1721057
+ */
+public class LineChart_AWT extends ApplicationFrame{
+    public LineChart_AWT( String applicationTitle , String chartTitle, XYDataset dataset) {
+        super(applicationTitle);
+        JFreeChart lineChart = ChartFactory.createXYLineChart(
+           chartTitle,
+           "Treinamento","Erros de classificação",
+           dataset);
+
+        ChartPanel chartPanel = new ChartPanel( lineChart );
+        chartPanel.setPreferredSize( new java.awt.Dimension( 1280 , 670 ) );
+
+        final XYPlot plot = (XYPlot) lineChart.getXYPlot( );
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer( );
+        lineChart.getPlot().setBackgroundPaint(Color.WHITE);
+        renderer.setSeriesPaint( 0 , Color.BLACK );
+        renderer.setSeriesStroke( 0 , new BasicStroke( 0.8f ) );
+        renderer.setSeriesPaint( 1 , Color.GREEN );
+        renderer.setSeriesStroke( 1 , new BasicStroke( 3.0f ) );
+        renderer.setSeriesPaint( 2 , Color.RED );
+        renderer.setSeriesStroke( 2 , new BasicStroke( 3.0f ) );
+
+//        renderer.setSeriesShapesVisible(0, false);
+//        renderer.setSeriesShapesVisible(1, false);
+        renderer.setSeriesLinesVisible(2, false);
+
+        XYPlot xyPlot = lineChart.getXYPlot();
+        org.jfree.chart.axis.ValueAxis domainAxis = xyPlot.getDomainAxis();
+        org.jfree.chart.axis.ValueAxis rangeAxis = xyPlot.getRangeAxis();
+
+//        rangeAxis.setAutoRange(true);
+        plot.setRenderer(renderer);
+        setContentPane( chartPanel ); 
+   }   
+}
